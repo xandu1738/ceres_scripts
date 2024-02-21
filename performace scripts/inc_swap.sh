82e08948dae6f4552a1cb31bb673afd3236989e5
xandu <xandu1738@gmail.com>
2024-02-21 15:00:04 +0300

    making user define tables to use in data dict::specifying the size of the swap to increase by as command argument

--- performace scripts/inc_swap.sh
+++ performace scripts/inc_swap.sh
@@ -1,7 +1,9 @@
 #! /usr/bin/bash
+
+size=$1
 swapon --show
 sudo swapoff /swapfile
-sudo fallocate -l 25G /swapfile
+sudo fallocate -l $sizeG /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
 free -h
